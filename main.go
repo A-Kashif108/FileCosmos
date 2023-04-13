3396b0b6fca8623f37dcbca3ed9ccb31cdc42cc6
drcode03 <ijatingrover@gmail.com>
2023-04-14 02:57:41 +0530

    crud_implementation

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+import (
+	"log"
+	"fmt"
+	"net/http"
+	"golang-crud-rest-api/controllers"
+	"golang-crud-rest-api/database"
+	"github.com/gorilla/mux"
+)
+
+func RegisterProductRoutes(router *mux.Router) {
+	router.HandleFunc("/api/products", controllers.GetProducts).Methods("GET")
+	router.HandleFunc("/api/products/{id}", controllers.GetProductById).Methods("GET")
+	router.HandleFunc("/api/products", controllers.CreateProduct).Methods("POST")
+	router.HandleFunc("/api/products/{id}", controllers.UpdateProduct).Methods("PUT")
+	router.HandleFunc("/api/products/{id}", controllers.DeleteProduct).Methods("DELETE")
+}
+
+func main() {
+	// Load Configurations from config.json using Viper
+	LoadAppConfig()
+	// Initialize Database
+	database.Connect(AppConfig.ConnectionString)
+	database.Migrate()
+	
+	// Initialize the router
+	router := mux.NewRouter().StrictSlash(true)
+	// Register Routes
+	RegisterProductRoutes(router)
+	// Start the server
+	log.Println(fmt.Sprintf("Starting Server on port %s", AppConfig.Port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", AppConfig.Port), router))
+}
